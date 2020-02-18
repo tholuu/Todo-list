63248cf66ed42c3f9ad3a428395da308883a4ce6
ThoLV <luuvietthothb@gmail.com>
2020-02-18 23:01:24 +0700

    conditonal rendering use if else or &&

--- src/App.js
+++ src/App.js
@@ -12,15 +12,39 @@ class App extends Component {
     ];
   }
   render() {
+    //conditional rendering use if else
+    // if (this.todoItems.length > 0) {
+    //   return (
+    //     <div className="App">
+    //       {
+    //         this.todoItems.map((item, index) =>
+    //           <TodoItem key={index} item={item} />)
+    //       }
+    //     </div>
+    //   );
+    // }
+    // else {
+    //   return (
+    //     <div className="App">'nothing here'</div>
+    //   )
+    // }
+
+    //conditional redering use &&
     return (
       <div className="App">
         {
-          this.todoItems.map((item, index) =>
+          this.todoItems.length > 0 && this.todoItems.map((item, index) =>
             <TodoItem key={index} item={item} />)
         }
+
+        {
+          this.todoItems.length === 0 && 'nothing here'
+        }
       </div>
-    );
+    )
+
   }
 }
+
 
 export default App;
