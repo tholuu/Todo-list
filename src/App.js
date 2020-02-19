447c410a08ddc127030c071f8bc7e2628fc6365a
ThoLV <luuvietthothb@gmail.com>
2020-02-19 23:58:11 +0700

    setState hander even click

--- src/App.js
+++ src/App.js
@@ -5,12 +5,39 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
-    this.todoItems = [
-      { title: "đi chợ", isComplete: true },
-      { title: "đi học" },
-      { title: "đi chơi" }
-    ];
+    this.state = {
+      todoItems: [
+        { title: "đi chợ" },
+        { title: "đi học" },
+        { title: "đi chơi" },
+        { title: "đi đú", }
+      ]
+    }
+
+    this.onItemClick = this.onItemClick.bind(this)
   }
+
+  onItemClick(item) {
+    return (event) => {
+      console.log('item')
+      const isComplete = item.isComplete
+      const { todoItems } = this.state
+
+      const index = todoItems.indexOf(item)
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      });
+    };
+  }
+
+
   render() {
     //conditional rendering use if else
     // if (this.todoItems.length > 0) {
@@ -30,21 +57,26 @@ class App extends Component {
     // }
 
     //conditional redering use &&
+    const { todoItems } = this.state
+    // console.log(this.state.item)
+
     return (
-      <div className="App">
+      < div className="App" >
         {
-          this.todoItems.length > 0 && this.todoItems.map((item, index) =>
-            <TodoItem key={index} item={item} />)
+          todoItems.length > 0 && todoItems.map((item, index) =>
+            <TodoItem
+              onClick={this.onItemClick(item)}
+              key={index}
+              item={item}
+            />)
         }
 
         {
-          this.todoItems.length === 0 && 'nothing here'
+          todoItems.length === 0 && 'nothing here'
         }
-      </div>
+      </div >
     )
-
   }
 }
-
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 import classNames from 'classnames/'
 import './TodoItem.css';
 class TodoItem extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    const { item } = this.props;
+    const { item, onClick } = this.props;
     //dynamic class names use js
     // let className = 'TodoItem';
     // if (item.isComplete) {
     //   className += " TodoItem-complete";
     // }
+
     return (
       //dynamic class use npm package classnames
-      <div className={classNames('TodoItem', { 'TodoItem-complete': this.props.item.isComplete })}>
+      <div onClick={onClick} className={classNames('TodoItem', { 'TodoItem-complete': item.isComplete === true })}>
         <p>
           {this.props.item.title}
         </p>
